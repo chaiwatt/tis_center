e67c29a3eb3c3844c4a4945a04294b9dfeb26d2a
chaiwatt <chaiwat_thaweechan@yahoo.com>
2025-02-16 11:57:04 +0700

    update

--- app/Http/Controllers/MyTestController.php
+++ app/Http/Controllers/MyTestController.php
@@ -829,7 +829,7 @@ class MyTestController extends Controller
             // แปลง JsonResponse เป็น array
             $resultArray = $result->getData(true);
 
-            // dd($resultArray);
+            dd($resultArray);
             
             // ตรวจสอบค่า message
             if (!empty($resultArray['message']) && $resultArray['message'] === true) {
